205d290c803989c145cb5311d2739482e60186b7
mithunthakkar8 <122671015+mithunthakkar8@users.noreply.github.com>
2025-01-10 19:35:54 +0530

    Update Zeros and Ones.py

--- Python/Numpy/Zeros and Ones.py
+++ Python/Numpy/Zeros and Ones.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-# Read input values for the shape of the array and convert them into a tuple of integers
+# Read input values for the dimensions of the array and convert them into a tuple of integers
 dimensions = tuple(map(int, input().split()))
 
-# Create a NumPy array filled with zeros, having the shape 'l', and with integer data type
+# Create a NumPy array filled with zeros, having the specified dimensions, and with integer data type
 print(np.zeros(dimensions, dtype=np.int))
 
-# Create a NumPy array filled with ones, having the shape 'l', and with integer data type
+# Create a NumPy array filled with ones, having the specified dimensions, and with integer data type
 print(np.ones(dimensions, dtype=np.int))
